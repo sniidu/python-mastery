5c8030b4842edccdfd398461c35575ce651f686e
sniidu <sniidu@users.noreply.github.com>
2023-09-03 21:18:26 +0300

    4.2 full

---
+++ validate.py
@@ -0,0 +1,107 @@
+class Validator:
+    @classmethod
+    def check(cls, value):
+        return value
+
+
+class Typed(Validator):
+    expected_type = object
+
+    @classmethod
+    def check(cls, value):
+        if not isinstance(value, cls.expected_type):
+            raise TypeError(f"Expected {cls.expected_type}")
+        return super().check(value)
+
+
+class Positive(Validator):
+    @classmethod
+    def check(cls, value):
+        if value < 0:
+            raise ValueError("Expected >= 0")
+        return super().check(value)
+
+
+class NonEmpty(Validator):
+    @classmethod
+    def check(cls, value):
+        if len(value) == 0:
+            raise ValueError("Must be non-empty")
+        return super().check(value)
+
+
+class Integer(Typed):
+    expected_type = int
+
+
+class Float(Typed):
+    expected_type = float
+
+
+class String(Typed):
+    expected_type = str
+
+
+class PositiveInteger(Integer, Positive):
+    pass
+
+
+class PositiveFloat(Float, Positive):
+    pass
+
+
+class NonEmptyString(String, NonEmpty):
+    pass
+
+
+class Stock:
+    _types = [str, int, float]
+    __slots__ = ["name", "_shares", "_price"]
+
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+    def __repr__(self):
+        # Neat oneliner for this ought to be working
+        return f"{self.__class__.__name__}({','.join(str(getattr(self, s)) for s in self.__init__.__code__.co_names)})"
+
+    def __eq__(self, other):
+        return isinstance(other, Stock) and (
+            (self.name, self.shares, self.price)
+            == (other.name, other.shares, other.price)
+        )
+
+    @classmethod
+    def from_row(cls, row):
+        """Alternative initiator"""
+        values = [func(val) for func, val in zip(cls._types, row)]
+        return cls(*values)
+
+    @property
+    def cost(self):
+        return self.shares * self.price
+
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        Typed.check(self._types[1])
+        PositiveInteger.check(value)
+        self._shares = value
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        Typed.check(self._types[2])
+        PositiveInteger.check(value)
+        self._price = value
+
+    def sell(self, num_of_shares: int):
+        self.shares -= num_of_shares
