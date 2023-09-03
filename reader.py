6c39619083ad12f74b439b3a8011b5b6c3718367
sniidu <sniidu@users.noreply.github.com>
2023-09-03 21:16:03 +0300

    4.2 done

--- reader.py
+++ reader.py
@@ -1,26 +1,44 @@
 import csv
+from abc import ABC, abstractmethod
 
-def read_csv_as_dicts(filename:str, types:list):
 
-    records = []
-    with open(filename, 'r') as f:
-        data = csv.reader(f)
-        headers = next(data)
+class CSVParser(ABC):
+    def parse(self, filename):
+        records = []
+        with open(filename) as f:
+            rows = csv.reader(f)
+            headers = next(rows)
+            for row in rows:
+                record = self.make_record(headers, row)
+                records.append(record)
+        return records
 
-        for row in data:
-            record = { name: func(val) for name, func, val in zip(headers, types, row) }
-            records.append(record)
+    @abstractmethod
+    def make_record(self, headers, row):
+        pass
 
-    return records
+
+class DictCSVParser(CSVParser):
+    def __init__(self, types):
+        self.types = types
+
+    def make_record(self, headers, row):
+        return {name: func(val) for name, func, val in zip(headers, self.types, row)}
+
+
+class InstanceCSVParser(CSVParser):
+    def __init__(self, cls):
+        self.cls = cls
+
+    def make_record(self, headers, row):
+        return self.cls.from_row(row)
+
 
 def read_csv_as_instances(filename, cls):
-    '''
-    Read a CSV file into a list of instances
-    '''
-    records = []
-    with open(filename) as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-        for row in rows:
-            records.append(cls.from_row(row))
-    return records
+    parser = InstanceCSVParser(cls)
+    return parser.parse(filename)
+
+
+def read_csv_as_dicts(filename: str, types: list):
+    parser = DictCSVParser(types)
+    return parser.parse(filename)
--- tableformat.py
+++ tableformat.py
@@ -1,17 +1,94 @@
-class TableFormatter:
+from abc import ABC, abstractmethod
+
+
+class TableFormatter(ABC):
+    @abstractmethod
     def headings(self, headers):
         raise NotImplementedError()
 
+    @abstractmethod
     def row(self, rowdata):
         raise NotImplementedError()
-    
-def print_table(seq:list, attr:list):
-    print(' '.join('%10s' % field for field in attr))
-    print(('-' * 10 + ' ') * len(attr))
-    for s in seq:
-        print(' '.join('%10s' % getattr(s, field) for field in attr))
 
-if __name__ == '__main__':
-    import stock
-    portfolio = stock.read_portfolio('Data/portfolio.csv')
-    print_table(portfolio, ['name','price'])
+
+class TextTableFormatter(TableFormatter):
+    def headings(self, headers):
+        print(" ".join("%10s" % h for h in headers))
+        print(("-" * 10 + " ") * len(headers))
+
+    def row(self, rowdata):
+        print(" ".join("%10s" % d for d in rowdata))
+
+
+class CsvTableFormatter(TableFormatter):
+    def headings(self, headers):
+        print(",".join(h for h in headers))
+
+    def row(self, rowdata):
+        print(",".join(str(d) for d in rowdata))
+
+
+class HTMLTableFormatter(TableFormatter):
+    def headings(self, headers):
+        print("<tr> <th>", "</th> <th>".join(h for h in headers), "</th> </tr>", sep="")
+
+    def row(self, rowdata):
+        print(
+            "<tr> <td>",
+            "</td> <td>".join(str(d) for d in rowdata),
+            "</td> </tr>",
+            sep="",
+        )
+
+
+class ColumnFormatMixin:
+    formats = []
+
+    def row(self, rowdata):
+        rowdata = [(fmt % d) for fmt, d in zip(self.formats, rowdata)]
+        super().row(rowdata)
+
+
+class UpperHeadersMixin:
+    # Heading is first passed to this which
+    # super's it to next class in uppercase
+    # class PortfolioFormatter(UpperHeadersMixin, TextTableFormatter)
+    def headings(self, headers):
+        super().headings([h.upper() for h in headers])
+
+
+def create_formatter(type: str, column_formats=None, upper_headers=None):
+    map = {
+        "html": HTMLTableFormatter,
+        "csv": CsvTableFormatter,
+        "text": TextTableFormatter,
+    }
+    formatter = map[type]
+    if column_formats:
+
+        class formatter(ColumnFormatMixin, formatter):
+            formats = column_formats
+
+    if upper_headers:
+
+        class formatter(UpperHeadersMixin, formatter):
+            pass
+
+    return formatter()
+
+
+def print_table(records, fields, formatter):
+    if not isinstance(formatter, TableFormatter):
+        raise TypeError("Expected a TableFormatter")
+    formatter.headings(fields)
+    for r in records:
+        rowdata = [getattr(r, fieldname) for fieldname in fields]
+        formatter.row(rowdata)
+
+
+if __name__ == "__main__":
+    import stock, reader, tableformat
+
+    portfolio = reader.read_csv_as_instances("Data/portfolio.csv", stock.Stock)
+    formatter = tableformat.create_formatter("csv")
+    tableformat.print_table(portfolio, ["name", "shares", "price"], formatter)
